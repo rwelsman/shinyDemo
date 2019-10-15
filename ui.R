eab09331a3782a5091e053980204e54b8d16c5a8
rwelsman <ryw@cypress.com>
2019-10-14 19:38:30 -0700

    updates

--- ui.R
+++ ui.R
@@ -32,7 +32,19 @@ shinyUI(fluidPage(
         mainPanel(
             plotOutput("scatterPlot"),
             h3("Regression Coefficients"),
-            tableOutput("dataTable")
+            tableOutput("dataTable"),
+            
+            h3("This R Shiny demo runs linear regression on the built-in mtcars dataset."),  
+            h3("Use the Dependent Variable pick list to choose which variable to use as the dependent variable."),
+            h3("The independent variable will always be mpg (miles per gallon)."),
+            h3(" "),
+            h3("After making your selection in the pick list the following will happen:"),
+            h3(" "),
+            h3("   1.The scatter plot will change to display your chosen independent variable on the x-axis"),
+            h3("   2.The linear regression line is plotted on the scatter chart"),
+            h3("   3.The summary of the intercept and slope coefficients are shown in the table below the scatter chart"),
+            h3(" "),
+            h2("Have fun!")
         )
     )
 ))
